f077a212c7970bdb6ee346c03bd66e559093dc86
Sazzadul Ahsan Sabbir <154515848+sazzadul-ahsan-sabbir@users.noreply.github.com>
2025-12-11 23:57:03 +0600

    Linkedlist

---
+++ Arraysearching.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+cin<<"n"<<endl;
+int search(int arr[],int size,int key){
+    for(int i=0;i<size;i++){
+     if(arr[i]==key)
+     return 1;
+     return -1;
+    }
+    int main(){
+        
+    }
+}
